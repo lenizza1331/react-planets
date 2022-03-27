d1ed967745efcd93467be4e620dccb3b27cea520
lenizza1331 <leniyuva@gmail.com>
2022-03-27 07:48:36 +0300

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
+import { data } from "./data";
+import { useState } from "react";
 
 function App() {
+
+const [planet, setPlanet] = useState(0);
+const {id, title, image, description} = data[planet];
+const [showMore, setShowMore] = useState(false);
+
+
+
+
+const goBack =  ()=>{
+  setPlanet((planet => {
+    planet--;
+    if (planet < 0){
+      return data.length - 1;
+    }
+    return planet
+  }))
+}
+
+const goNext = ()=>{
+  setPlanet((planet=>{
+    planet++;
+    if (planet > data.length-1){
+      planet = 0;
+    }
+    return planet;
+  }))
+}
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className='container'>
+    <div className='page' key={id}>
+
+
+      {/* <div className='center'>
+      <h1>Планеты солнечной системы</h1>
+      </div> */}
+
+      <div className='center'>
+        <h2>{title}</h2>
+      </div>
+
+      <div className='center picture'>
+        <img src={image} alt="solar system" />
+      </div>
+
+      <div className='center'>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {showMore ? description : description.substring(0,310)+"..."} <button className='btn' onClick={()=> setShowMore(!showMore)}> { showMore ? "Скрыть текст..." :  "Читать дальше..."} </button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+
+      <div className='between'>
+        <button onClick={goBack}>Назад</button>
+        <button onClick={goNext}>Вперед</button>
+      </div>
+
+
+    </div>
     </div>
   );
 }
